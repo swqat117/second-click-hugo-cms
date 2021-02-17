1b9cbb8d59b6c26f43f46aeb44ae45fa712e359c
swqat117 <akshay@turftown.in>
2021-02-17 12:38:47 +0530

    pass

--- src/js/cms-preview-templates/bulletin.js
+++ src/js/cms-preview-templates/bulletin.js
@@ -29,7 +29,7 @@ export default class ValuesPreview extends React.Component {
       image = window.parent.location.protocol + "//" + window.parent.location.host + image;
     }
     
-    const entryValues = entry.getIn(["data", "values"]);
+    const entryValues = entry.getIn(["data", "bulletin"]);
     const values = entryValues ? entryValues.toJS() : [];
     
     return <div>
